41e610128ec1fa31df75b98287481d29f81bce5a
ProgramKingAbel <abelmorara254@gmail.com>
2023-05-24 14:33:04 +0100

    Activate event listeners for submit and refresh

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { getData } from './modules/apiData';
+import postUserData from './modules/userInput';
 import './styles/main.css';
+const form = document.getElementById('dataForm');
+const refresh = document.getElementById('refresh');
+
+form.addEventListener('submit', postUserData);
+refresh.addEventListener('click', getData);
