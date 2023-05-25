5bb62a72fb6d3cb94678a0d70eefd409d277b9d5
ProgramKingAbel <abelmorara254@gmail.com>
2023-05-25 11:10:33 +0100

    Enforce Data integrity on form - no empty field

--- src/modules/userInput.js
+++ src/modules/userInput.js
@@ -5,13 +5,15 @@ const postUserData = (e) => {
   const pName = document.getElementById('name').value;
   const pScore = document.getElementById('score').value;
 
-  const player = {
-    user: pName,
-    score: pScore,
-  };
+  if (pName && pScore) {
+    const player = {
+      user: pName,
+      score: pScore,
+    };
+    sendData(player); 
+    document.getElementById('dataForm').reset();
+  }
 
-  sendData(player);
-  document.getElementById('dataForm').reset();
 };
 
 export default postUserData;
